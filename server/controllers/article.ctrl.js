36ed077642a88f6a090f8c8eef3e96c644f6b92c
ahmed essmat <ahmedabdelkayoum@gmail.com>
2018-09-21 19:59:07 +0200

    create routers

--- server/controllers/article.ctrl.js
+++ server/controllers/article.ctrl.js
@@ -1,6 +1,6 @@
 /** server/controllers/article.ctrl.js*/
 const Article = require('./../models/Article')
-const User = require('./../models/User')
+const User = require('../models/User')
 const fs = require('fs')
 const cloudinary = require('cloudinary')
 module.exports = {
